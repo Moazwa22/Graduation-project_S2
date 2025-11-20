02cd8ac5039a09ab153b5445149a11d03f3fb3ba
Moaz <moaazw84@gmail.com>
2025-11-20 02:59:20 +0200

    initial commit - baseTest, HomePage, HomeTests

---
+++ src/test/java/baseTest/baseTest.java
@@ -0,0 +1,24 @@
+package baseTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class baseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost/opencart/");
+    }
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
+}
