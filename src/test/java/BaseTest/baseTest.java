9eef14fcc212961be27242fb65ef9492bb4d240b
Moaz <moaazw84@gmail.com>
2025-11-20 04:15:05 +0200

    Update: add before class

--- src/test/java/BaseTest/baseTest.java
+++ src/test/java/BaseTest/baseTest.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class baseTest {
     protected WebDriver driver;
 
-    @BeforeMethod
+    @BeforeClass
     public void setup(){
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
@@ -21,7 +22,7 @@ public class baseTest {
         driver.get("http://localhost/opencart/");
     }
 
-    @AfterMethod
+    @AfterClass
     public void teardown(){
         driver.quit();
     }
