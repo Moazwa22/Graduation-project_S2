9ced4208c2b1310bef8e79c760ecfa81881e2eec
Tebrawy <boodytebry@gmail.com>
2025-11-25 20:36:23 +0200

    temp

---
+++ src/test/java/Pages/CheckOut/CheckoutPage.java
@@ -0,0 +1,15 @@
+package Pages.CheckOut;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutPage {
+
+    WebDriver driver;
+
+    By cartIconLocator = By.
+
+    public CheckoutPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
---
+++ src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,29 @@
+package StepDefinitions;
+
+import Pages.HomePage.HomePage;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Hooks {
+
+    public static WebDriver driver;
+    public HomePage homePage;
+
+    @Before
+    public void setup(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://demo.opencart.com/");
+    }
+
+    @After
+    public void teardown(){
+        driver.quit();
+    }
+
+}
