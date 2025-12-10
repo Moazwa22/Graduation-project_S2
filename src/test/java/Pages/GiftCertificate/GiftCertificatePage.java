c06052bba5b9843b0ce91ab987acd65f3ce4370a
Tebrawy <boodytebry@gmail.com>
2025-12-10 17:03:27 +0200

    my work

--- src/test/java/Pages/GiftCertificate/GiftCertificatePage.java
+++ src/test/java/Pages/GiftCertificate/GiftCertificatePage.java
@@ -1,4 +1,20 @@
 package Pages.GiftCertificate;
 
+import StepDefinitions.Hooks;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class GiftCertificatePage {
+
+    WebDriver driver;
+    WebDriverWait wait = new WebDriverWait(Hooks.driver, Duration.ofSeconds(10));
+
+    public GiftCertificatePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
 }
