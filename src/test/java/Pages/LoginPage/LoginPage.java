926ab3d68ccc9617177f9f3c08cfa7d1341d38ed
Tebrawy <boodytebry@gmail.com>
2025-12-10 13:11:33 +0200

    Your meaningful commit message

--- src/test/java/Pages/LoginPage/LoginPage.java
+++ src/test/java/Pages/LoginPage/LoginPage.java
@@ -23,7 +23,7 @@ public class LoginPage {
     By accountPageLocator = By.tagName("h1");
     String expectedTitleOfMyAccountPage =  "My Account";
 
-    String email = "test10@gmail.com";
+    String email = "test9@test.com";
     String password = "testtest";
 
     public LoginPage(WebDriver driver) {
